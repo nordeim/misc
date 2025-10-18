170c86db8631f831d0e3fe5f66a8a0761cb26da2
nordeim <nordeimkuahsziyp@outlook.com>
2025-10-18 19:54:36 +0800

    Create bun_install.sh

---
+++ Docker/bun_install.sh
@@ -0,0 +1,7 @@
+mkdir -p /home/devuser/.bun/bin
+curl --fail --location --progress-bar --output /home/devuser/.bun/bin/bun.zip https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64-musl.zip
+unzip -oqd /home/devuser/.bun/bin /home/devuser/.bun/bin/bun.zip
+mv /home/devuser/.bun/bin/bun-linux-x64-musl/bun /home/devuser/.bun/bin/bun
+chmod +x /home/devuser/.bun/bin/bun
+rm -r /home/devuser/.bun/bin/bun-linux-x64-musl /home/devuser/.bun/bin/bun.zip
+tildify /home/devuser/.bun/bin/bun
