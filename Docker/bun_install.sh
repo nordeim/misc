4c49082857ce0184b15e5184fb4ea2da4894fb01
nordeim <nordeimkuahsziyp@outlook.com>
2025-10-18 21:00:25 +0800

    Update bun_install.sh

--- Docker/bun_install.sh
+++ Docker/bun_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # https://copilot.microsoft.com/shares/pvV3qpx111LHiV7HG4GFE
 mkdir -p /home/devuser/.bun/bin
-curl --fail --location --progress-bar --output /home/devuser/.bun/bin/bun.zip https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64-musl.zip
+curl --fail --location --progress-bar --output /home/devuser/.bun/bin/bun.zip https://github.com/oven-sh/bun/releases/download/bun-v1.3.0/bun-linux-x64-musl.zip
+# https://github.com/oven-sh/bun/releases/latest/download/bun-linux-x64-musl.zip
 unzip -oqd /home/devuser/.bun/bin /home/devuser/.bun/bin/bun.zip
 mv /home/devuser/.bun/bin/bun-linux-x64-musl/bun /home/devuser/.bun/bin/bun
 chmod +x /home/devuser/.bun/bin/bun
