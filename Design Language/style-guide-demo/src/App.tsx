530d5679a955331cf82038a4a8b52c6018809e99
nordeim <nordeimkuahsziyp@outlook.com>
2025-11-14 14:29:29 +0800

    Create App.tsx

---
+++ Design Language/style-guide-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StyleGuideDashboard from './components/StyleGuideDashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-backgroundMain">
+      <StyleGuideDashboard />
+    </div>
+  );
+}
+
+export default App;
